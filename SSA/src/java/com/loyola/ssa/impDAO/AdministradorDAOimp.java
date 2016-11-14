c12c25a4eca451a35483657a6cb72696d9528b4f
Carmenlore <carmenlorenagonzales@gmail.com>
2016-11-14 18:55:57 -0400

    Add files via upload

---
+++ SSA/src/java/com/loyola/ssa/impDAO/AdministradorDAOimp.java
@@ -0,0 +1,33 @@
+
+package com.loyola.ssa.impDAO;
+
+import com.loyola.ssa.DAO.AdministradorDAO;
+import com.loyola.ssa.entities.Administrador;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class AdministradorDAOimp implements AdministradorDAO{
+
+    @Override
+    public ArrayList<Administrador> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Administrador getAdministrador(Integer id_administrador) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void RegistrarID_admin(Administrador administrador) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void RegistrarFecha_creacion(Administrador administrador) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
