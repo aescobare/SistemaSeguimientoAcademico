a00b3c94e5489098865cfc0b3c51caf63efac672
Carmenlore <carmenlorenagonzales@gmail.com>
2016-11-14 18:49:39 -0400

    Add files via upload
    Agregue la clase administrador con el patrón DAO

---
+++ SistemaSeguimientoAcademico/SSA/src/java/com/loyola/ssa/DAO/AdministradorDAO.java
@@ -0,0 +1,31 @@
+package com.loyola.ssa.DAO;
+
+import java.util.ArrayList;
+import com.loyola.ssa.entities.Administrador;
+import java.util.ArrayList;
+import java.sql.*;
+import com.loyola.ssa.ConexionDB.DBconexion;
+public interface AdministradorDAO {
+    public ArrayList<Administrador>getAll();
+    public Administrador getAdministrador(Integer id_administrador);
+    public void RegistrarID_admin(Administrador administrador);
+    public void RegistrarFecha_creacion(Administrador administrador);
+    
+    
+    
+    /**public void RegistrarID_admin(Administrador administrador){
+    DBconexion conexion=new DBconexion();
+    try{
+    Statement estatuto=conexion.getConnection().createStatement();
+    estatuto.executeUpdate("INSERT INTO administrador VALUES ('"+administrador.getId_administrador()+"','"+administrador.getFecha_creacion()+"')");
+    JOptionPane.showMessageDialog(null,"se registro correctamente","Informacio",JOptionPane.INFORMATION_MESSAGE);
+    estatuto.close();
+    conexion.desconectar();}
+    catch(SQLException e)
+    {
+        System.out.println(e.getMessage());
+        JOptionPane.showMessageDialog(null,"no se registro");
+    }
+    }*/
+    
+}
