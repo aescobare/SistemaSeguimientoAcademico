bc74ce773e200f935af495efa90a8c26c34f180d
FERCHEX-DELL\FERNANDO <blackdracon2006@gmail.com>
2016-11-16 00:58:44 -0400

    Patron Composite

---
+++ SistemaSeguimientoAcademico/SSA/src/java/patrones/composite/ManagerComposite.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patrones.composite;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @author FERNANDO
+ */
+public class ManagerComposite implements Empleado{
+    
+    private String Nombre;
+    
+    private double Salario;
+    
+    public ManagerComposite(String nombre,double salario){
+        this.Nombre=nombre;
+        this.Salario=salario;
+        
+    }
+    List<Empleado> empleados = new ArrayList<Empleado>();
+    
+    public void add(Empleado empleado){
+        empleados.add(empleado);
+    }
+    public String getNombre(){
+        return Nombre;
+    }
+    
+    public double getSalario(){
+        return Salario;
+    }
+    
+    public void print(){
+        System.out.println("Gerente");
+        System.out.println("Nombre:"+getNombre());
+        System.out.println("Salario:"+getSalario());
+        System.out.println("----------");
+        
+        Iterator<Empleado> empleadoIterator=empleados.iterator();
+        while(empleadoIterator.hasNext()){
+            Empleado empleado=empleadoIterator.next();
+            empleado.print();
+        }
+        
+    }
+
+
+}
+
