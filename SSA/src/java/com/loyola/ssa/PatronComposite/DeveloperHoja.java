8cd4cee388f3118f350dd000768276e79ee9f0a6
ferchex <blackdracon2006@gmail.com>
2016-11-15 20:52:25 -0400

    Add files via upload

---
+++ SSA/src/java/com/loyola/ssa/PatronComposite/DeveloperHoja.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PatronComposite;
+
+/**
+ *
+ * @author FERNANDO
+ */
+public class DeveloperHoja implements Empleado{
+    
+    private String Nombre;
+    private double Salario;
+    
+    public DeveloperHoja(String nombre, double salario){
+        this.Nombre=nombre;
+        this.Salario=salario;
+        
+    }
+
+   
+    
+    
+    public void add(Empleado empleado){
+        
+    }
+    
+    public String getNombre(){
+        return Nombre;
+        
+    }
+    public double getSalario(){
+        return Salario;
+        
+    }
+    
+    public void print(){
+        System.out.println("Empleado");
+        System.out.println("Nombre:"+getNombre());
+        System.out.println("Salario:"+getSalario());
+        System.out.println("----------");
+    }
+
+
+
+
+}
