ff084a16d0cfdd5f4918b1bd5a16e11e00b7d194
DESKTOP-ASVDPH8\ALVARO <stronguista_23.campeon@hotmail.com>
2016-11-19 21:33:23 -0700

    no message

---
+++ SSA/src/java/com/loyola/ssa/PatronStrategy/AreaCirculo.java
@@ -0,0 +1,27 @@
+package com.loyola.ssa.PatronStrategy;
+
+import patron.strategy.interfaces.IArea;
+
+// Estrategia para calcular el área de un círculo.
+public class AreaCirculo implements IArea {
+
+    private final float radio;
+
+    // Constructor.
+    public AreaCirculo(float radio) {
+        this.radio = radio;
+    }
+
+    // Cálculo del área de un círculo.
+    @Override
+    public float calculaArea() {
+        return (float) (Math.PI * Math.pow(radio, 2));
+    }
+
+    @Override
+    public String getResultado() {
+        return "El área del círculo es "+calculaArea();
+    }
+}
+
+
