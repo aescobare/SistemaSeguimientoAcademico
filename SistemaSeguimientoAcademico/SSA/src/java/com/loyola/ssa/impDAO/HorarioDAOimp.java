83f6f18180a7faf2194a0d20b6dde9990026f165
FERCHEX-DELL\FERNANDO <blackdracon2006@gmail.com>
2016-11-23 13:55:47 -0400

    Implementacion horario

---
+++ SistemaSeguimientoAcademico/SSA/src/java/com/loyola/ssa/impDAO/HorarioDAOimp.java
@@ -0,0 +1,80 @@
+
+package com.loyola.ssa.impDAO;
+
+import com.loyola.ssa.DAO.HorarioDAO;
+import com.loyola.ssa.entities.horario;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class HorarioDAOimp implements HorarioDAO{
+   
+    @Override
+    public ArrayList<horario> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public horario gethorario(Integer Id_horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void RegistrarId_horario(horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void EditarId_horario(horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void EliminarId_horario(horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void Registrarhorario_inicio(horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void Editarhorario_inicio(horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+   public void Eliminarhorario_inicio(horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void Registrarhora_fin(horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void Editarhora_fin(horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+     public void Eliminarhora_fin(horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+     public void Registrarturno(horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void Editarturno(horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void Elimarturno(horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void Registrardia(horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void Editardia(horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void Eliminardia(horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
