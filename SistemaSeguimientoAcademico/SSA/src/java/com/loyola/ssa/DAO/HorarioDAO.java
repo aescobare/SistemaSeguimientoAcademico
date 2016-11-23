3f96e8a5b1607a08e77a049f66928fbcc1151138
FERCHEX-DELL\FERNANDO <blackdracon2006@gmail.com>
2016-11-23 12:29:06 -0400

    Interfaz HorarioDao Completo

--- SistemaSeguimientoAcademico/SSA/src/java/com/loyola/ssa/DAO/HorarioDAO.java
+++ SistemaSeguimientoAcademico/SSA/src/java/com/loyola/ssa/DAO/HorarioDAO.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.loyola.ssa.DAO;
 
-/**
- *
- * @author FERNANDO
- */
+import java.util.ArrayList;
+import com.loyola.ssa.entities.horario;
+import java.util.ArrayList;
+import java.sql.*;
+import com.loyola.ssa.ConexionDB.DBconexion;
 public interface HorarioDAO {
     
+    public ArrayList<horario>getAll();
+    public horario gethorario(Integer Id_horario);
+    public void RegistrarId_horario(horario horario);
+    public void EditarId_horario(horario horario);
+    public void EliminarId_horario(horario horario);
+    public void Registrarhorario_inicio(horario horario);
+    public void Editarhorario_inicio(horario horario);
+    public void Eliminarhorario_inicio(horario horario);
+    public void Registrarhora_fin(horario horario);
+    public void Editarhora_fin(horario horario);
+    public void Eliminarhora_fin(horario horario);
+    public void Registrarturno(horario horario);
+    public void Editarturno(horario horario);
+    public void Elimarturno(horario horario);
+    public void Registrardia(horario horario);
+    public void Editardia(horario horario);
+    public void Eliminardia(horario horario);
 }
