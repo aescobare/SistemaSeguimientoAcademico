c0b08b59e5e9264939e2dcbda8088b76275f58e3
DESKTOP-ASVDPH8\ALVARO <stronguista_23.campeon@hotmail.com>
2016-11-22 17:03:11 -0700

    patron de estrategia

--- SSA/src/java/com/loyola/ssa/entities/notas.java
+++ SSA/src/java/com/loyola/ssa/entities/notas.java
@@ -1,23 +1,30 @@
 package com.loyola.ssa.entities;
 import java.util.Date;
 
-public class notas {
+public class notas extends Estudiante {
 
     private int   id_nota;
-    private int   id_estudiante;
+    //private int   id_estudiante;
     private float   valor;
     private Date     fecha;
-    private int   id_docente;
-    private int   id_usuario;
+   // private int   id_docente;
+   // private int   id_usuario;
    
-
+////constructor
     public notas(int id_nota, int id_estudiante, float valor, Date fecha, int id_docente, int id_usuario) {
+        super(id_usuario,);
         this.id_nota = id_nota;
-        this.id_estudiante = id_estudiante;
+        
         this.valor = valor;
         this.fecha = fecha;
-        this.id_docente = id_docente;
-        this.id_usuario = id_usuario;
+        
+        Docente docente;
+    }
+    
+    
+    public void asociar_docente()
+    {
+        docente.agregar_docente;
     }
     public int getId_nota() {
         return id_nota;
@@ -27,13 +34,7 @@ public class notas {
         this.id_nota = id_nota;
     }
 
-    public int getId_estudiante() {
-        return id_estudiante;
-    }
-
-    public void setId_estudiante(int id_estudiante) {
-        this.id_estudiante = id_estudiante;
-    }
+    
 
     public void getvalor() {
         float valor;
@@ -51,21 +52,7 @@ public class notas {
         this.fecha = fecha;
     }
 
-    public int getId_docente() {
-        return id_docente;
-    }
-
-    public void setId_docente(int id_docente) {
-        this.id_docente = id_docente;
-    }
-
-    public int getId_usuario() {
-        return id_usuario;
-    }
-
-    public void setId_usuario(int id_usuario) {
-        this.id_usuario = id_usuario;
-    }
+  
 
     
 }
