36ff35068b2be8bc45588c146994e1b25df0438c
chriscuaquira <christiancuaquira@gmail.com>
2016-12-01 00:22:52 -0400

    Add files via upload

---
+++ aplicacionWebInscripcion/src/inscripcion/inscripcion1.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inscripcion;
+
+/**
+ *
+ * @author Christian
+ */
+public class inscripcion1 {
+ 
+    private String   Id_inscripcion;
+    private String  Fecha_inscripcion;
+    private String   Id_estudiante;
+    private String   Id_carrera;
+    private String   Tipo_inscripcion;
+   
+
+    public void setId_inscripcion(String Id_inscripcion) {
+        this.Id_inscripcion = Id_inscripcion;
+    }
+
+    public void setFecha_inscripcion(String Fecha_inscripcion) {
+        this.Fecha_inscripcion = Fecha_inscripcion;
+    }
+
+    public void setId_estudiante(String Id_estudiante) {
+        this.Id_estudiante = Id_estudiante;
+    }
+
+    public void setId_carrera(String Id_carrera) {
+        this.Id_carrera = Id_carrera;
+    }
+
+    public void setTipo_inscripcion(String Tipo_inscripcion) {
+        this.Tipo_inscripcion = Tipo_inscripcion;
+    }
+
+    public String getId_inscripcion() {
+        return Id_inscripcion;
+    }
+      public String getFecha_inscripcion() {
+        return Fecha_inscripcion;
+    }
+
+    public String getId_estudiante() {
+        return Id_estudiante;
+    }
+
+    public String getId_carrera() {
+        return Id_carrera;
+    }
+
+    public String getTipo_inscripcion() {
+        return Tipo_inscripcion;
+    }
+    
+}
