b32d5191d12d074ee859bd91f66bc8cea73c300e
chriscuaquira <christiancuaquira@gmail.com>
2016-11-30 23:45:34 -0400

    Update inscripcion1.java

--- Inscripcion/src/inscripcion/inscripcion1.java
+++ Inscripcion/src/inscripcion/inscripcion1.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package inscripcion;
 
-/**
- *
- * @author Christian
- */
+
 public class inscripcion1 {
  
     private String   Id_inscripcion;
